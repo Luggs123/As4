7d18c4e62f0bbdc89f136166b8cba08039570fc9
CommanderMark <commanderpro100@gmail.com>
2016-12-02 17:36:26 -0500

    Added cringe loop.

--- src/pack_main/SandboxFX.java
+++ src/pack_main/SandboxFX.java
@@ -10,7 +10,6 @@ import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -34,6 +33,7 @@ public class SandboxFX extends Application implements pack_pet.InterfacePet {
 	final static SpriteAnimation houseAnimation = new SpriteAnimation(houseImageView, Duration.millis(1200), 40, 8, 0, 0, 410, 449);
 
 	public void start(Stage primaryStage) {
+		
 		primaryStage.setTitle("Assignment 4");
 		VBox mainPane = new VBox();
 		HBox menuButtons = new HBox(15);
@@ -43,23 +43,21 @@ public class SandboxFX extends Application implements pack_pet.InterfacePet {
 		 * SPRITE ANIMATIONS *
 		 *********************/
 		// Initializing the pets and their animation objects.
-		ClsPet dog = new ClsPet("Dog", Duration.millis(1200), 3, 3, 0, 2245, SRITE_WIDTH, SRITE_WIDTH);
+		ClsPet dog = new ClsPet("Dog", Duration.millis(600), 3, 3, 0, 2245, SRITE_WIDTH, SRITE_WIDTH);
 		dog.getPetAnim().setCycleCount(Animation.INDEFINITE);
 		dog.getPetAnim().setAutoReverse(true);
 		dog.getPetImage().setX(200 - 16);
 		dog.getPetImage().setY(40 - 16);
-		dog.getPetImage().setScaleX(1);
-		dog.getPetImage().setScaleY(1);
 		dog.getPetImage().setOpacity(0);
 		
-		ClsPet cat = new ClsPet("Cat", Duration.millis(800), 3, 3, 96, 2245, SRITE_WIDTH, SRITE_WIDTH);
+		ClsPet cat = new ClsPet("Cat", Duration.millis(400), 3, 3, 96, 2245, SRITE_WIDTH, SRITE_WIDTH);
 		cat.getPetAnim().setCycleCount(Animation.INDEFINITE);
 		cat.getPetAnim().setAutoReverse(true);
 		cat.getPetImage().setX(800 - 16);
 		cat.getPetImage().setY(40 - 16);
 		cat.getPetImage().setOpacity(0);
 		
-		ClsPet mouse = new ClsPet("Mouse", Duration.millis(600), 3, 3, 192, 2245, SRITE_WIDTH, SRITE_WIDTH);
+		ClsPet mouse = new ClsPet("Mouse", Duration.millis(300), 3, 3, 192, 2245, SRITE_WIDTH, SRITE_WIDTH);
 		mouse.getPetAnim().setCycleCount(Animation.INDEFINITE);
 		mouse.getPetAnim().setAutoReverse(true);
 		mouse.getPetImage().setX(800 - 16);
@@ -153,83 +151,136 @@ public class SandboxFX extends Application implements pack_pet.InterfacePet {
 		/**************
 		 * SEQUENCE 2 *
 		 **************/
-		// Fade in the arrows.
-		ImageView arrow1a = getArrow();
-		arrow1a.setX(340);
-		arrow1a.setY(60);
-		FadeTransition arrow1aFade = getArrowFade(arrow1a);
+		// Create the arrows and their fading animations.
+		ImageView arrows[] = new ImageView[9];
+		ParallelTransition arrowsFade[] = new ParallelTransition[]{ new ParallelTransition(), new ParallelTransition(), new ParallelTransition() };
 		
-		ImageView arrow2a = getArrow();
-		arrow2a.setX(450);
-		arrow2a.setY(60);
-		FadeTransition arrow2aFade = getArrowFade(arrow2a);
+		for (int i = 0; i < arrows.length; i++) {
+			arrows[i] = new ImageView(SPRITE_SHEET);
+			arrows[i].setViewport(new Rectangle2D(322, 2262, 80, 61));
+			arrows[i].setOpacity(0);
+			
+			FadeTransition arrowFade = new FadeTransition(Duration.millis(2000), arrows[i]);
+			arrowFade.setCycleCount(1);
+			arrowFade.setFromValue(0);
+			arrowFade.setToValue(1);
+			
+			// Every arrow's fade animation is added in a way so that the first arrow in each second fades in in parallel.
+			arrowsFade[i % 3].getChildren().add(arrowFade);
 		
-		ImageView arrow3a = getArrow();
-		arrow3a.setX(560);
-		arrow3a.setY(60);
-		FadeTransition arrow3aFade = getArrowFade(arrow3a);
+			// Determine the coordinates of each arrow.
+			int x = 0, y = 0;
+			
+			switch (i) {
+			
+			// Second set of arrows.
+			case 3:
+			case 4:
+			case 5: {
+				x = 720;
+				y = 100 + ((i - 3) * 100);
+			} break;
+			
+			// Third set of arrows.
+			case 6:
+			case 7:
+			case 8: { 
+				x = 600 - ((i - 6) * 110);
+				y = 460;
+				arrows[i].setRotate(90);
+			} break;
+			
+			// First set of arrows.
+			default: {
+				x = 340 + (i * 110);
+				y = 60;
+				arrows[i].setRotate(180);
+			}
+			}
+			
+			arrows[i].setX(x);
+			arrows[i].setY(y);
+		}
 		
-		ImageView arrow1b = getArrow();
-		arrow1b.setX(720);
-		arrow1b.setY(100);
-		arrow1b.setRotate(90);
-		FadeTransition arrow1bFade = getArrowFade(arrow1b);
+//		ImageView arrow1a = getArrow();
+//		arrow1a.setX(340);
+//		arrow1a.setY(60);
+//		FadeTransition arrow1aFade = getArrowFade(arrow1a);
+//		
+//		ImageView arrow2a = getArrow();
+//		arrow2a.setX(450);
+//		arrow2a.setY(60);
+//		FadeTransition arrow2aFade = getArrowFade(arrow2a);
+//		
+//		ImageView arrow3a = getArrow();
+//		arrow3a.setX(560);
+//		arrow3a.setY(60);
+//		FadeTransition arrow3aFade = getArrowFade(arrow3a);
+//		
+//		ImageView arrow1b = getArrow();
+//		arrow1b.setX(720);
+//		arrow1b.setY(100);
+//		arrow1b.setRotate(90);
+//		FadeTransition arrow1bFade = getArrowFade(arrow1b);
+//		
+//		ImageView arrow2b = getArrow();
+//		arrow2b.setX(720);
+//		arrow2b.setY(200);
+//		arrow2b.setRotate(90);
+//		FadeTransition arrow2bFade = getArrowFade(arrow2b);
+//		
+//		ImageView arrow3b = getArrow();
+//		arrow3b.setX(720);
+//		arrow3b.setY(300);
+//		arrow3b.setRotate(90);
+//		FadeTransition arrow3bFade = getArrowFade(arrow3b);
+//		
+//		ImageView arrow1c = getArrow();
+//		arrow1c.setX(600);
+//		arrow1c.setY(460);
+//		arrow1c.setRotate(180);
+//		FadeTransition arrow1cFade = getArrowFade(arrow1c);
+//		
+//		ImageView arrow2c = getArrow();
+//		arrow2c.setX(490);
+//		arrow2c.setY(460);
+//		arrow2c.setRotate(180);
+//		FadeTransition arrow2cFade = getArrowFade(arrow2c);
+//		
+//		ImageView arrow3c = getArrow();
+//		arrow3c.setX(380);
+//		arrow3c.setY(460);
+//		arrow3c.setRotate(180);
+//		FadeTransition arrow3cFade = getArrowFade(arrow3c);
 		
-		ImageView arrow2b = getArrow();
-		arrow2b.setX(720);
-		arrow2b.setY(200);
-		arrow2b.setRotate(90);
-		FadeTransition arrow2bFade = getArrowFade(arrow2b);
+//		ParallelTransition arrow1 = new ParallelTransition(arrow1aFade, arrow1bFade, arrow1cFade);
+//		ParallelTransition arrow2 = new ParallelTransition(arrow2aFade, arrow2bFade, arrow2cFade);
+//		ParallelTransition arrow3 = new ParallelTransition(arrow3aFade, arrow3bFade, arrow3cFade);
 		
-		ImageView arrow3b = getArrow();
-		arrow3b.setX(720);
-		arrow3b.setY(300);
-		arrow3b.setRotate(90);
-		FadeTransition arrow3bFade = getArrowFade(arrow3b);
-		
-		ImageView arrow1c = getArrow();
-		arrow1c.setX(600);
-		arrow1c.setY(460);
-		arrow1c.setRotate(180);
-		FadeTransition arrow1cFade = getArrowFade(arrow1c);
-		
-		ImageView arrow2c = getArrow();
-		arrow2c.setX(490);
-		arrow2c.setY(460);
-		arrow2c.setRotate(180);
-		FadeTransition arrow2cFade = getArrowFade(arrow2c);
-		
-		ImageView arrow3c = getArrow();
-		arrow3c.setX(380);
-		arrow3c.setY(460);
-		arrow3c.setRotate(180);
-		FadeTransition arrow3cFade = getArrowFade(arrow3c);
-		
-		ParallelTransition arrow1 = new ParallelTransition(arrow1aFade, arrow1bFade, arrow1cFade);
-		ParallelTransition arrow2 = new ParallelTransition(arrow2aFade, arrow2bFade, arrow2cFade);
-		ParallelTransition arrow3 = new ParallelTransition(arrow3aFade, arrow3bFade, arrow3cFade);
-		
-		seq_2 = new SequentialTransition(arrow1, arrow2, arrow3);
+		seq_2 = new SequentialTransition(arrowsFade[0], arrowsFade[1], arrowsFade[2]);
 		seq_2.setCycleCount(1);
                 
-                /**************
+		/**************
 		 * SEQUENCE 3 *
 		 **************/
-                TranslateTransition dogTranslate = new TranslateTransition(Duration.millis(5000),dog.getPetImage());
-                dogTranslate.setToX(600);
-                
-                TranslateTransition catTranslate = new TranslateTransition(Duration.millis(5000),cat.getPetImage());
-                catTranslate.setToY(400);
-                
-                TranslateTransition mouseTranslate = new TranslateTransition(Duration.millis(5000),mouse.getPetImage());
-                mouseTranslate.setToX(-600);
-                        
-                seq_3 = new ParallelTransition(dogTranslate,catTranslate,mouseTranslate);
-                
+		TranslateTransition dogTranslate = new TranslateTransition(Duration.millis(6000), dog.getPetImage());
+		dogTranslate.setToX(600);
+
+		TranslateTransition catTranslate = new TranslateTransition(Duration.millis(6000), cat.getPetImage());
+		catTranslate.setToY(400);
+
+		TranslateTransition mouseTranslate = new TranslateTransition(Duration.millis(6000), mouse.getPetImage());
+		mouseTranslate.setToX(-600);
+
+		seq_3 = new ParallelTransition(dogTranslate, catTranslate, mouseTranslate);
+
 		mainSeq = new SequentialTransition(seq_1, seq_2, seq_3/*, seq_4, seq_5*/);
 		
 		// Add the panes to the main window.
-		mainAnimation.getChildren().addAll(path, dog.getPetImage(), cat.getPetImage(), mouse.getPetImage(), houseImageView, arrow1a, arrow2a, arrow3a, arrow1b, arrow2b, arrow3b, arrow1c, arrow2c, arrow3c);
+		mainAnimation.getChildren().addAll(path, dog.getPetImage(), cat.getPetImage(), mouse.getPetImage(), houseImageView);
+		for (int i = 0; i < 9; i++) {
+			mainAnimation.getChildren().add(arrows[i]);
+		}
 		mainPane.getChildren().addAll(mainAnimation, menuButtons);
 
 		Scene scene = new Scene(mainPane, 1000, 800);
@@ -246,22 +297,5 @@ public class SandboxFX extends Application implements pack_pet.InterfacePet {
 	public static void play() {
 		animSeqs.play();
 		mainSeq.play();
-	}
-	
-	private static ImageView getArrow() {
-		ImageView arrow = new ImageView(SPRITE_SHEET);
-		arrow.setViewport(new Rectangle2D(322, 2262, 80, 61));
-		arrow.setOpacity(0);
-		
-		return arrow;
-	}
-	
-	private static FadeTransition getArrowFade(ImageView arrow) {
-		FadeTransition arrowFade = new FadeTransition(Duration.millis(2000), arrow);
-		arrowFade.setCycleCount(1);
-		arrowFade.setFromValue(0);
-		arrowFade.setToValue(1);
-		
-		return arrowFade;
 	}
 }
