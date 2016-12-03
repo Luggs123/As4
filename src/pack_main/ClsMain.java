fa1019d9881b1af98d73b6bfef785ac543d75f52
CommanderMark <commanderpro100@gmail.com>
2016-12-02 22:24:17 -0500

    Added sequence 4 and 5.

--- src/pack_main/ClsMain.java
+++ src/pack_main/ClsMain.java
@@ -3,16 +3,20 @@ package pack_main;
 import javafx.animation.Animation;
 import javafx.animation.FadeTransition;
 import javafx.animation.ParallelTransition;
+import javafx.animation.PathTransition;
+import javafx.animation.PathTransitionBuilder;
 import javafx.animation.ScaleTransition;
 import javafx.animation.SequentialTransition;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.ArcTo;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
@@ -21,7 +25,7 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 import pack_pet.ClsPet;
 
-public class SandboxFX extends Application implements pack_pet.InterfacePet {
+public class ClsMain extends Application implements pack_pet.InterfacePet {
 	
 	// Primary animation objects.
 	static Animation seq_1, seq_2, seq_3, seq_4, seq_5;
@@ -74,7 +78,7 @@ public class SandboxFX extends Application implements pack_pet.InterfacePet {
 		animSeqs = new ParallelTransition(dog.getPetAnim(), cat.getPetAnim(), mouse.getPetAnim(), houseAnimation);
 		
 		// Generate the path.
-		Path path = PathBuilder.create()
+		Path mainPath = PathBuilder.create()
 				.elements(
 						new MoveTo(200, 40),
 						new LineTo(800, 40),
@@ -202,61 +206,6 @@ public class SandboxFX extends Application implements pack_pet.InterfacePet {
 			arrows[i].setY(y);
 		}
 		
-//		ImageView arrow1a = getArrow();
-//		arrow1a.setX(340);
-//		arrow1a.setY(60);
-//		FadeTransition arrow1aFade = getArrowFade(arrow1a);
-//		
-//		ImageView arrow2a = getArrow();
-//		arrow2a.setX(450);
-//		arrow2a.setY(60);
-//		FadeTransition arrow2aFade = getArrowFade(arrow2a);
-//		
-//		ImageView arrow3a = getArrow();
-//		arrow3a.setX(560);
-//		arrow3a.setY(60);
-//		FadeTransition arrow3aFade = getArrowFade(arrow3a);
-//		
-//		ImageView arrow1b = getArrow();
-//		arrow1b.setX(720);
-//		arrow1b.setY(100);
-//		arrow1b.setRotate(90);
-//		FadeTransition arrow1bFade = getArrowFade(arrow1b);
-//		
-//		ImageView arrow2b = getArrow();
-//		arrow2b.setX(720);
-//		arrow2b.setY(200);
-//		arrow2b.setRotate(90);
-//		FadeTransition arrow2bFade = getArrowFade(arrow2b);
-//		
-//		ImageView arrow3b = getArrow();
-//		arrow3b.setX(720);
-//		arrow3b.setY(300);
-//		arrow3b.setRotate(90);
-//		FadeTransition arrow3bFade = getArrowFade(arrow3b);
-//		
-//		ImageView arrow1c = getArrow();
-//		arrow1c.setX(600);
-//		arrow1c.setY(460);
-//		arrow1c.setRotate(180);
-//		FadeTransition arrow1cFade = getArrowFade(arrow1c);
-//		
-//		ImageView arrow2c = getArrow();
-//		arrow2c.setX(490);
-//		arrow2c.setY(460);
-//		arrow2c.setRotate(180);
-//		FadeTransition arrow2cFade = getArrowFade(arrow2c);
-//		
-//		ImageView arrow3c = getArrow();
-//		arrow3c.setX(380);
-//		arrow3c.setY(460);
-//		arrow3c.setRotate(180);
-//		FadeTransition arrow3cFade = getArrowFade(arrow3c);
-		
-//		ParallelTransition arrow1 = new ParallelTransition(arrow1aFade, arrow1bFade, arrow1cFade);
-//		ParallelTransition arrow2 = new ParallelTransition(arrow2aFade, arrow2bFade, arrow2cFade);
-//		ParallelTransition arrow3 = new ParallelTransition(arrow3aFade, arrow3bFade, arrow3cFade);
-		
 		seq_2 = new SequentialTransition(arrowsFade[0], arrowsFade[1], arrowsFade[2]);
 		seq_2.setCycleCount(1);
                 
@@ -273,11 +222,74 @@ public class SandboxFX extends Application implements pack_pet.InterfacePet {
 		mouseTranslate.setToX(-600);
 
 		seq_3 = new ParallelTransition(dogTranslate, catTranslate, mouseTranslate);
-
-		mainSeq = new SequentialTransition(seq_1, seq_2, seq_3/*, seq_4, seq_5*/);
+		
+		/**************
+		 * SEQUENCE 4 *
+		 **************/
+		// Fade out all the objects on-screen.
+		FadeTransition dogFadeOut = new FadeTransition(Duration.millis(2500), dog.getPetImage());
+		dogFadeOut.setCycleCount(1);
+		dogFadeOut.setFromValue(1);
+		dogFadeOut.setToValue(0);
+		
+		FadeTransition catFadeOut = new FadeTransition(Duration.millis(2500), cat.getPetImage());
+		catFadeOut.setCycleCount(1);
+		catFadeOut.setFromValue(1);
+		catFadeOut.setToValue(0);
+		
+		FadeTransition mouseFadeOut = new FadeTransition(Duration.millis(2500), mouse.getPetImage());
+		mouseFadeOut.setCycleCount(1);
+		mouseFadeOut.setFromValue(1);
+		mouseFadeOut.setToValue(0);
+		
+		FadeTransition houseFadeOut = new FadeTransition(Duration.millis(2500), houseImageView);
+		houseFadeOut.setCycleCount(1);
+		houseFadeOut.setFromValue(1);
+		houseFadeOut.setToValue(0);
+		
+		seq_4 = new ParallelTransition(dogFadeOut, catFadeOut, mouseFadeOut, houseFadeOut);
+		
+		for (int i = 0; i < 9; i++) {
+			FadeTransition arrowFadeOut = new FadeTransition(Duration.millis(2500), arrows[i]);
+			arrowFadeOut.setCycleCount(1);
+			arrowFadeOut.setFromValue(1);
+			arrowFadeOut.setToValue(0);
+			((ParallelTransition) seq_4).getChildren().add(arrowFadeOut);
+		}
+		
+		/**************
+		 * SEQUENCE 5 *
+		 **************/
+		// Display the ending label.
+		Label lblNotice = new Label("WOW CRINGE");
+		lblNotice.setOpacity(0);
+		
+		FadeTransition lblFade = new FadeTransition(Duration.millis(1000), lblNotice);
+		lblFade.setCycleCount(1);
+		lblFade.setFromValue(0);
+		lblFade.setToValue(1);
+		
+		Path seq_5Path = PathBuilder.create()
+				.elements(
+						new MoveTo(500, 40),
+						new ArcTo(350, 250, 0, 750, 200, true, true),
+						new ArcTo(100, 150, 0, 250, 200, true, false),
+						new ArcTo(100, 100, 0, 500, 400, true, false))
+						.build();
+		
+		PathTransition labelAnim = PathTransitionBuilder.create()
+				.duration(Duration.millis(2000))
+				.path(seq_5Path)
+				.node(lblNotice)
+				.build();
+		
+		seq_5 = new ParallelTransition(lblFade, labelAnim);
+		
+		// Add all the sequences to the main animation sequence.
+		mainSeq = new SequentialTransition(seq_1, seq_2, seq_3, seq_4, seq_5);
 		
 		// Add the panes to the main window.
-		mainAnimation.getChildren().addAll(path, dog.getPetImage(), cat.getPetImage(), mouse.getPetImage(), houseImageView);
+		mainAnimation.getChildren().addAll(mainPath, dog.getPetImage(), cat.getPetImage(), mouse.getPetImage(), houseImageView, lblNotice);
 		for (int i = 0; i < 9; i++) {
 			mainAnimation.getChildren().add(arrows[i]);
 		}
